c01cb0622384620ce19767d0c7feecba5f07a8aa
t-phoenix <abhijaipur2011@gmail.com>
2026-04-24 17:13:52 +0530

    fix: font

--- adpr-website/src/components/Hero.jsx
+++ adpr-website/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
             <div className="container">
                 <div className="row align-items-center justify-content-end">
                     <div className="col-md-12">
-                        <p className="mb-12 sub-heading" style={{ fontSize: '1.4rem', marginBottom: '1.6rem', fontWeight: '500' }}>
+                        <p className="mb-12 sub-heading" style={{ fontSize: '1.2rem', marginBottom: '1.6rem', fontWeight: '300' }}>
                             ADPR is named for the twin pillars of brand building and communications:
                             advertising and public relations. <br /> Whatever the channel, whoever the audience, and with the addition of
                             innovative web3 creative services, <br />ADPR puts these disciplines to work for you.
@@ -19,8 +19,8 @@ const Hero = () => {
                                 <li className="list-inline-item"><a href="https://www.linkedin.com/in/anand-peter-916b863/" target="_blank" className="p-2"><i className="fa fa-linkedin"></i></a></li>
                                 <li className="list-inline-item"><a href="https://www.behance.net/adpr142" target="_blank" className="p-2"><i className="fa fa-behance"></i></a></li>
                             </ul>
-                            <p style={{ textAlign: 'center', marginTop: '40px', color: '#fff', fontSize: '1.2rem', fontWeight: '500' }}>
-                                Check out our new initiative <a href="https://memetic.adpr.work/" target="_blank" rel="noopener noreferrer" style={{ color: '#4ba0ff', textDecoration: 'none' }}>adpr Memetic Brand Labs</a>
+                            <p style={{ textAlign: 'center', marginTop: '40px', color: '#fff', fontSize: '1.1rem', fontWeight: '200' }}>
+                                Check out our new initiative <a href="https://memetic.adpr.work/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', textDecoration: 'underline', textDecorationColor: '#fff' }}>adpr Memetic Brand Labs</a>
                             </p>
                         </div>
                     </div>
